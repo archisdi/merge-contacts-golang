a3c5d03e3a6fadf2f3fec6e5541c349a3aa2c8f5
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-12-04 18:27:10 +0700

    read json file

--- contact.go
+++ contact.go
@@ -1,5 +1,19 @@
 package main
 
-func merge(data StringMap) string {
-	return "archie"
+// StringMap exported
+type StringMap map[string]interface{}
+
+// Contact exported
+type Contact struct {
+	fullname   string
+	email      StringMap
+	phone      StringMap
+	address    StringMap
+	otherField StringMap
+}
+
+// MergeContact exported
+func MergeContact(contacts []StringMap) []Contact {
+	mergedContacts := []Contact{}
+	return mergedContacts
 }
--- main.go
+++ main.go
@@ -1,32 +1,23 @@
 package main
 
-import "fmt"
-
-// StringMap exported
-type StringMap map[string]string
-
-// Contact exported
-type Contact struct {
-	fullname   string
-	email      StringMap
-	phone      StringMap
-	address    StringMap
-	otherField StringMap
-}
-
-// MergeContact exported
-func MergeContact(contacts []StringMap) []Contact {
-	mergedContacts := []Contact{}
-	return mergedContacts
-}
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
-	contact := Contact{
-		fullname: "archie isdiningrat",
-		email: StringMap{
-			"personal": "archie.isdiningrat@gmail.com",
-		},
+	JSONFile, err := os.Open("sample/contacts.json")
+	if err != nil {
+		fmt.Println(err)
 	}
+	defer JSONFile.Close()
 
-	fmt.Println(contact)
+	byteValue, _ := ioutil.ReadAll(JSONFile)
+
+	var rawContacts []StringMap
+	json.Unmarshal([]byte(byteValue), &rawContacts)
+
+	fmt.Println(rawContacts)
 }
